47cea4a9bc98e5cb415c519ca42fd47525d4f722
Hogan McDonald <hpeter.mcdonald@icloud.com>
2017-12-05 22:28:22 -0600

    day 1 solved

--- src/com/hoganmcdonald/Main.java
+++ src/com/hoganmcdonald/Main.java
@@ -3,6 +3,7 @@ package com.hoganmcdonald;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	int day1 = Day1.solve();
+	System.out.println(day1);
     }
 }
